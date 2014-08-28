44f33b3d354fcecb3b3f11b1a8887c9d4d02a149
musicakc <amitoj1606@gmail.com>
2014-08-28 19:54:57 +0530

    Initial work on scraping CS229

---
+++ WebScraper/scraper/scraper/spiders/dmoz_spider.py
@@ -0,0 +1,22 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+
+class DmozSpider(BaseSpider):
+	name="dmoz"
+	allowed_domains=["dmoz.org"]
+	start_urls=[
+	"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+def parse(self,response):
+	hxs=HtmlXPathSelector(response)
+	sites=hxs.select('//ul/li')
+	items=[]
+	for site in sites:
+		item=PDFItem()
+		item['title']=site.select('a/text()').extract()
+		item['link']=site.select('a/@href').extract()
+		item['desc'] = site.select('text()').extract()
+		items.append(item)
+	return items
