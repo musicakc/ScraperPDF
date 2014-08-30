9c8041f62aff737b019bfe9a2c7fa8c082cfb7fc
musicakc <amitoj1606@gmail.com>
2014-08-30 20:20:52 +0530

    Started from scratch again

---
+++ WebScraper/cs229/cs229/settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for cs229 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'cs229'
+
+SPIDER_MODULES = ['cs229.spiders']
+NEWSPIDER_MODULE = 'cs229.spiders'
+'''ITEM_PIPELINES = [
+    'cs229.files.FilesPipeline',
+]'''
+FILES_STORE = '/home/amitoj/Projects/Scrapy/PDFScraper/cs229/cs229/downloads'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'cs229 (+http://www.yourdomain.com)'
