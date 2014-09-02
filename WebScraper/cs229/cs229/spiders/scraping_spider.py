c01b9e252b006a5ba5873baed5951efe6b282821
musicakc <amitoj1606@gmail.com>
2014-09-02 23:56:33 +0530

    Some comments added for readability

--- WebScraper/cs229/cs229/spiders/scraping_spider.py
+++ WebScraper/cs229/cs229/spiders/scraping_spider.py
@@ -1,3 +1,8 @@
+'''
+Made by : Amitoj Kaur Chawla
+Date: 01 September 2014
+'''
+
 from scrapy.spider import BaseSpider
 from scrapy.contrib.spiders import CrawlSpider,Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
@@ -8,22 +13,23 @@ from scrapy.selector import HtmlXPathSelector
 class Cs229Spider(CrawlSpider):
     name = "cs229"
     allowed_domains = ["cs229.stanford.edu"]
-    start_urls = [
+    start_urls = [ ## url to download pdf files from
         "http://cs229.stanford.edu/projects2013.html"
     ]
-    rules = (Rule(SgmlLinkExtractor(allow_domains=('http://cs229.stanford.edu/projects2013.html', )), callback='parse_urls', follow=True),)
+    rules = (Rule(SgmlLinkExtractor(allow_domains=('http://cs229.stanford.edu/projects2013.html', )), callback='parse_urls', follow=True),) 
 
 	
 
 def parse_urls(self, response):
-	hxs=HtmlXPathSelector(response)
+	hxs=HtmlXPathSelector(response) ##extracting xpath
 	item=Cs229Item()
 	item['pdf_urls']=hxs.select('//a/@href')
 	pdf_urls=hxs.select('//a/@href').extract()
 	for url in pdf_urls :
 		yield scrapy.Request(url,callback=self.save_pdf)
 
-def save_pdf(self,response):
+def save_pdf(self,response): 
+	## save pdf files
 	path=self.get_path(item['url'])
 	with open(path,"wb") as f:
 		f.write(response.body) 
